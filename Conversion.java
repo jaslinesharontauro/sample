5084423b51675505be281dcde93774c7c0b83e08
jaslinesharontauro <53473310+jaslinesharontauro@users.noreply.github.com>
2020-04-22 13:11:58 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Conversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n, count = 0, a;
+        String x = "";
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter any decimal number:");
+        n = s.nextInt();
+        while(n > 0)
+        {
+            a = n % 2;
+            if(a == 1)
+            {
+                count++;
+            }
+            x = a + "" + x;
+            n = n / 2;
+        }
+        System.out.println("Binary number:"+x);
+
+	}
+
+}
